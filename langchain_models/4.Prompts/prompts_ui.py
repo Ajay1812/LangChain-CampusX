8b4be8898b025f3eca23aed5bc706378ce146ee8
Ajay1812 <a.kumar01c@gmail.com>
2025-05-30 15:06:32 +0530

    load prompt by json file

--- langchain_models/4.Prompts/prompts_ui.py
+++ langchain_models/4.Prompts/prompts_ui.py
@@ -1,6 +1,6 @@
 from langchain.chains.summarize.refine_prompts import prompt_template
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 from dotenv import load_dotenv
 import streamlit as st
 import os
@@ -12,38 +12,24 @@ model = ChatGoogleGenerativeAI(model="gemini-1.5-flash-8b",
                                max_completion_tokens=10)
 
 st.header("Research tool 🔨")
-paper_input = st.selectbox("Select Research Paper Name",
-                           ["Attention Is All You Need",
-                            "BERT: Pre-training of Deep Bidirectional Transformers",
-                            "GPT-3: Language Models are Few-shot Learners",
-                            "Diffusion Models Ban GANs on Image, Synthesis"])
+paper_input = st.selectbox( "Select Research Paper Name",
+                            ["Attention Is All You Need",
+                             "BERT: Pre-training of Deep Bidirectional Transformers",
+                             "GPT-3: Language Models are Few-Shot Learners",
+                             "Diffusion Models Beat GANs on Image Synthesis"])
 
-style_input = st.selectbox("Select Explanation Style",
-                           ["Beginner-Friendly",
-                            "Technical",
-                            "Code-Oriented",
-                            "Mathematical"])
+style_input = st.selectbox( "Select Explanation Style",
+                            ["Beginner-Friendly",
+                             "Technical",
+                             "Code-Oriented",
+                             "Mathematical"] )
 
-length_input = st.selectbox("Select Explanation Length",
-                            ["Short (1-2 paragraphs)",
-                             "Medium (3-5 paragraphs)",
-                             "Long (Detailed Explanation)"])
+length_input = st.selectbox( "Select Explanation Length",
+                             ["Short (1-2 paragraphs)",
+                              "Medium (3-5 paragraphs)",
+                              "Long (detailed explanation)"] )
 
-template = PromptTemplate(
-    template="""
-    Please summarize the research paper titled "{paper_input}" with the following specifications:
-    Explanation Style: {style_input}  
-    Explanation Length: {length_input}  
-    1. Mathematical Details:
-        - Include relevant mathematical equations if present in the paper.
-        - Explain the mathematical concepts using simple, intuitive code snippets where applicable. ]
-    2. Analogies:   
-        - Use relatable analogies to simplify complex ideas.  
-    If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing. 
-    Ensure the summary is clear, accurate, and aligned with the provided style and length.\n,
-    """,
-    input_variables=["paper_input","style_input","length_input"]
-)
+template = load_prompt("template.json")
 
 prompt = template.invoke({
     "paper_input": paper_input,
